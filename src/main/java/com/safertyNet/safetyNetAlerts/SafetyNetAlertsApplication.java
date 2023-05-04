fc2b3810bd06a1f707e4373231c550df0684f563
Mickael Haye <mickael.haye@gmail.com>
2023-05-04 12:01:53 +0200

    Creation du projet

---
+++ src/main/java/com/safertyNet/safetyNetAlerts/SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safertyNet.safetyNetAlerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safertyNet/safetyNetAlerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safertyNet.safetyNetAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
