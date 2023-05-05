18e6e4795159a9664a7e215fa27e186f9df969f3
Mickael Haye <mickael.haye@gmail.com>
2023-05-05 15:00:17 +0200

    Je sais pas

--- src/main/java/com/safertyNet/safetyNetAlerts/model/Firestation.java
+++ src/main/java/com/safertyNet/safetyNetAlerts/model/Firestation.java
@@ -1,5 +1,25 @@
 package com.safertyNet.safetyNetAlerts.model;
 
+import lombok.Data;
+
+@Data
 public class Firestation {
+
+	private String address;
+	
+	private int station;
+
+	public Firestation(String address, int station) {
+		super();
+		this.address = address;
+		this.station = station;
+	}
+
+	@Override
+	public String toString() {
+		return "Firestation [address=" + address + ", station=" + station + "]";
+	}
+	
+	
 
 }
