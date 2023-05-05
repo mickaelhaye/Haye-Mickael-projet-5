713eef1b91f6fae6375666690486d8f424e6c667
Mickael Haye <mickael.haye@gmail.com>
2023-05-05 14:50:12 +0200

    creation model Person

---
+++ src/main/java/com/safertyNet/safetyNetAlerts/model/Person.java
@@ -0,0 +1,46 @@
+package com.safertyNet.safetyNetAlerts.model;
+
+import lombok.Data;
+
+@Data
+public class Person {
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String address;
+	
+	private String city;
+	
+	private String zip;
+	
+	private String phone;
+	
+	private String email;
+
+	public Person(String firstName, String lastName, String address, String city, String zip, String phone,
+			String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.zip = zip;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", zip=" + zip + ", phone=" + phone + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
