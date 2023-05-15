48b4a368e2fcc6dc1a6d8c206e0fdc2f721e40de
Mickael Haye <mickael.haye@gmail.com>
2023-05-15 10:38:07 +0200

    Creation d'un service calcul age

--- src/main/java/com/safetynet/safetynetalerts/model/FileEntryModel.java
+++ src/main/java/com/safetynet/safetynetalerts/model/FileEntryModel.java
@@ -1,12 +1,9 @@
 package com.safetynet.safetynetalerts.model;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
+
+import com.safetynet.safetynetalerts.service.CalculAgeService;
 
 import lombok.Data;
 
@@ -43,7 +40,8 @@ public class FileEntryModel {
 			for (MedicalrecordModel medicalrecords : medicalrecords) {
 				if ((person.getFirstName().equals(medicalrecords.getFirstName()))
 						&& (person.getLastName().equals(medicalrecords.getLastName()))) {
-					int age = calculAge(medicalrecords.getBirthdate());
+					CalculAgeService calcul = new CalculAgeService();
+					int age = calcul.calculAge(medicalrecords.getBirthdate());
 					if (age > 18) {
 						listPersonsPlus18.add(person);
 					} else {
@@ -59,62 +57,6 @@ public class FileEntryModel {
 		listObjects.add("le nombre d'enfants est de " + listPersons18EtMoins.size());
 
 		return listObjects;
-	}
-
-	public int calculAge(String birthdate) {
-		// calcul de l'age
-		// récupération date actuelle
-		Calendar calendar = new GregorianCalendar();
-		calendar.setTime(new Date());
-		String formatDate = "dd/MM/yyyy";
-		SimpleDateFormat sdf = new SimpleDateFormat(formatDate);
-		String dateActuelle = sdf.format(calendar.getTime());
-
-		Calendar calStr1 = Calendar.getInstance();
-		Calendar calStr2 = Calendar.getInstance();
-		Calendar calStr0 = Calendar.getInstance();
-		Date date1 = null;
-		Date date2 = null;
-		int nbMois = 0;
-		int nbAnnees = 0;
-		long nbJours = 0;
-
-		try {
-			date1 = new SimpleDateFormat("dd/MM/yyyy").parse(birthdate);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			date2 = new SimpleDateFormat("dd/MM/yyyy").parse(dateActuelle);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		calStr1.setTime(date1);
-		calStr2.setTime(date2);
-
-		nbMois = 0;
-		while (calStr1.before(calStr2)) {
-			calStr1.add(GregorianCalendar.MONTH, 1);
-			if (calStr1.before(calStr2) || calStr1.equals(calStr2)) {
-				nbMois++;
-			}
-		}
-		nbAnnees = (nbMois / 12);
-		/*
-		 * nbMois = (nbMois - (nbAnnees * DOUZE));
-		 * 
-		 * calStr0 = Calendar.getInstance(); calStr0.setTime(date1);
-		 * calStr0.add(GregorianCalendar.YEAR, nbAnnees);
-		 * calStr0.add(GregorianCalendar.MONTH, nbMois); nbJours =
-		 * (calStr2.getTimeInMillis() - calStr0.getTimeInMillis()) / 86400000;
-		 * 
-		 * System.out.print("Nb Annees : "+nbAnnees+"\n");
-		 * System.out.print("Nb Mois : "+nbMois+"\n");
-		 * System.out.print("Nb Jours : "+nbJours+"\n");
-		 */
-		return nbAnnees;
 	}
 
 }
---
+++ src/main/java/com/safetynet/safetynetalerts/service/CalculAgeService.java
@@ -0,0 +1,73 @@
+package com.safetynet.safetynetalerts.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.springframework.stereotype.Service;
+
+import lombok.Data;
+
+@Data
+@Service
+public class CalculAgeService {
+
+	public int calculAge(String birthdate) {
+		// calcul de l'age
+		// récupération date actuelle
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(new Date());
+		String formatDate = "dd/MM/yyyy";
+		SimpleDateFormat sdf = new SimpleDateFormat(formatDate);
+		String dateActuelle = sdf.format(calendar.getTime());
+
+		Calendar calStr1 = Calendar.getInstance();
+		Calendar calStr2 = Calendar.getInstance();
+		Calendar calStr0 = Calendar.getInstance();
+		Date date1 = null;
+		Date date2 = null;
+		int nbMois = 0;
+		int nbAnnees = 0;
+		long nbJours = 0;
+
+		try {
+			date1 = new SimpleDateFormat("dd/MM/yyyy").parse(birthdate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			date2 = new SimpleDateFormat("dd/MM/yyyy").parse(dateActuelle);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		calStr1.setTime(date1);
+		calStr2.setTime(date2);
+
+		nbMois = 0;
+		while (calStr1.before(calStr2)) {
+			calStr1.add(GregorianCalendar.MONTH, 1);
+			if (calStr1.before(calStr2) || calStr1.equals(calStr2)) {
+				nbMois++;
+			}
+		}
+		nbAnnees = (nbMois / 12);
+		/*
+		 * nbMois = (nbMois - (nbAnnees * DOUZE));
+		 * 
+		 * calStr0 = Calendar.getInstance(); calStr0.setTime(date1);
+		 * calStr0.add(GregorianCalendar.YEAR, nbAnnees);
+		 * calStr0.add(GregorianCalendar.MONTH, nbMois); nbJours =
+		 * (calStr2.getTimeInMillis() - calStr0.getTimeInMillis()) / 86400000;
+		 * 
+		 * System.out.print("Nb Annees : "+nbAnnees+"\n");
+		 * System.out.print("Nb Mois : "+nbMois+"\n");
+		 * System.out.print("Nb Jours : "+nbJours+"\n");
+		 */
+		return nbAnnees;
+	}
+
+}
