b2baf67dbe44d30d93649e2badc22b83ad2f6837
Mickael Haye <mickael.haye@gmail.com>
2023-05-16 11:50:06 +0200

    Suppression @autowired Controller

--- src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
@@ -1,19 +1,13 @@
 package com.safetynet.safetynetalerts;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.safetynet.safetynetalerts.controller.Controller;
 
 import lombok.Data;
 
 @Data
 @SpringBootApplication
 public class SafetyNetAlertsApplication {
-
-	@Autowired
-	private Controller controller;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
